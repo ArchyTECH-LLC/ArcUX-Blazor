7a3b7e8a6525275f7a30c73e07c409a0820d894f
JeffJacka <Jeff.Jacka@archy.tech>
2023-10-13 18:27:47 -0500

    Refactoring components into ArcComponent wrapper

---
+++ Source/Arc.UX/Components/ArcElement.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Arc.UX.Components;
+
+
+public interface IArcComponent
+{
+    public bool Visible { get; set; } 
+    public string? CssClasses { get; set; }
+    public string ComponentName { get; }
+}
+
+public abstract class ArcElement : ComponentBase, IArcComponent
+{
+    [Parameter]
+    public bool Visible { get; set; } = true;
+
+    [Parameter]
+    public string? CssClasses { get; set; }
+
+
+    public string ComponentName => GetType().Name;
+}
+
+
+public abstract class ArcInputBase<T>: InputBase<T>, IArcComponent
+{
+    [Parameter]
+    public bool Visible { get; set; } = true;
+
+    [Parameter]
+    public string? CssClasses { get; set; }
+
+
+    public string ComponentName => GetType().Name;
+}
