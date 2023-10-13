b4873dc426f7829a065b4e6f0ffcfe8a780eb25a
JeffJacka <Jeff.Jacka@archy.tech>
2023-10-13 17:49:01 -0500

    Adding ArcComponentWrapper

---
+++ Source/Arc.UX/Components/ArcComponent.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.AspNetCore.Components;
+
+namespace Arc.UX.Components;
+
+public abstract class ArcComponent : ComponentBase
+{
+    [Parameter]
+    public bool Visible { get; set; } = true;
+
+    [Parameter]
+    public string? CssClass { get; set; }
+
+    [Parameter]
+    public ElementStyle? Style { get; set; }
+
+    public string ComponentName => GetType().Name;
+}
---
+++ Source/Arc.UX/Extensions/CssExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Arc.UX.Extensions;
+
+internal static class CssExtensions
+{
+    public static string? ToLoweredCssClass<T>(this T? obj, string? prefix = "arc-")
+    {
+        return obj != null ? $"{prefix}{obj}" : null;
+    }
+
+    public static string? ToggleCssClass<T>(this T? obj, string cssClass)
+    {
+        return obj != null ? cssClass : null;
+    }
+
+}
